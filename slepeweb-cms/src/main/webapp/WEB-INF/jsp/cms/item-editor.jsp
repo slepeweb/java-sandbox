5facd32750399a5f17a4be0eb2e1f34f94fe8289
George Buttigieg <george@slepeweb.com>
2014-08-26 21:53:49 +0100

    tidied up modal dialogs

--- slepeweb-cms/src/main/webapp/WEB-INF/jsp/cms/item-editor.jsp
+++ slepeweb-cms/src/main/webapp/WEB-INF/jsp/cms/item-editor.jsp
@@ -65,7 +65,7 @@
 		<ul id="sortable-links">
 			<c:forEach items="${requestItem.inlinesAndRelations}" var="link">
 				<li class="ui-state-default"><span class="ui-icon ui-icon-arrowthick-2-n-s"></span><a 
-					href="/cms/editor/${link.child.id}">${link}</a>
+					href="${applicationContextPath}/page/editor/${link.child.id}">${link}</a>
 						<button class="remove-link float-right">Remove</button><span 
 							class="hide">${link.parentId},${link.child.id},${link.type},${link.name}</span></li>
 			</c:forEach>
@@ -137,3 +137,68 @@
 		</div>
 	</form>
 </div>
+
+<div id="dialog-update-success" class="hide" title="Item update complete">
+  <p>
+    <span class="ui-icon ui-icon-circle-check" style="float:left; margin:0 7px 50px 0;"></span>
+    You have successfully updated this item.
+  </p>
+</div>
+
+<div id="dialog-update-error" class="hide" title="Item update error">
+  <p>
+    <span class="ui-icon ui-icon-circle-check" style="float:left; margin:0 7px 50px 0;"></span>
+    <b class="fail">Oops - the item update failed.</b>
+  </p>
+</div>
+
+<div id="dialog-add-success" class="hide" title="New item created">
+  <p>choose
+    <span class="ui-icon ui-icon-circle-check" style="float:left; margin:0 7px 50px 0;"></span>
+    You have successfully added a new item.
+  </p>
+</div>
+
+<div id="dialog-add-error" class="hide" title="Failed to create new item">
+  <p>
+    <span class="ui-icon ui-icon-circle-check" style="float:left; margin:0 7px 50px 0;"></span>
+    Oops - the new item was not created.
+  </p>
+</div>
+
+<div id="dialog-trash-confirm" class="hide" title="Delete item?">
+	<p>
+		<span class="ui-icon ui-icon-alert" style="float:left; margin:0 7px 20px 0;"></span>
+		This will delete the current item PLUS ALL descendant items. Are you sure you want to do this?
+	</p>
+</div>
+
+<div id="dialog-trash-success" class="hide" title="Item trashed">
+  <p>
+    <span class="ui-icon ui-icon-circle-check" style="float:left; margin:0 7px 50px 0;"></span>
+    You have successfully moved the item(s) into the trash bin.
+  </p>
+</div>
+
+<div id="dialog-trash-error" class="hide" title="Failed to trash item">
+  <p>
+    <span class="ui-icon ui-icon-circle-check" style="float:left; margin:0 7px 50px 0;"></span>
+    Oops - no item(s) were moved into the trash bin.
+  </p>
+</div>
+
+<div id="dialog-choose-linktype" class="hide" title="Choose link type">
+  <p>
+    <span class="ui-icon ui-icon-circle-check" style="float:left; margin:0 7px 50px 0;"></span>
+    Please select the type of link you wish to create.
+  </p>
+</div>
+
+<div id="dialog-move-confirm" class="hide" title="Move item?">
+	<p>
+		<span class="ui-icon ui-icon-alert" style="float:left; margin:0 7px 20px 0;"></span>
+		You are about to move an item in the content structure. Are you sure you want to do this?
+	</p>
+</div>
+
+
--- slepeweb-cms/src/main/webapp/resources/js/editor.js
+++ slepeweb-cms/src/main/webapp/resources/js/editor.js
@@ -1,16 +1,27 @@
 $(function() {
-  // Get form field names and values for forms on item-editor 
-  var getFieldsFormInputData = function() {
-	  var result = {};
-	  $("#field-form input, #field-form textarea").each(function(i, obj) {
-		  result[$(obj).attr("name")] = $(obj).val();
-	  });
-	  return result;
-  };
+	// Get form field names and values for forms on item-editor 
+	var getFieldsFormInputData = function() {
+		var result = {};
+		$("#field-form input, #field-form textarea").each(function(i, obj) {
+			result[$(obj).attr("name")] = $(obj).val();
+		});
+		return result;
+	};
+	
+	var showDialog = function(id) {
+		$("#" + id).dialog({
+			modal: true,
+			buttons: {
+				Ok: function() {
+					$(this).dialog("close");
+				}
+			}
+		});
+	};
   
-  // (Re-)render the forms
-  var renderItemForms = function(nodeKey, activeTab) {
-	$.ajax(_ctx + "/rest/item/editor", {
+	// (Re-)render the forms
+	var renderItemForms = function(nodeKey, activeTab) {
+		$.ajax(_ctx + "/rest/item/editor", {
 			cache: false,
 			data: {key: nodeKey}, 
 			dataType: "html",
@@ -23,73 +34,98 @@ $(function() {
 				
 				// Re-build tabs 
 				if (tabsdiv.hasClass("ui-tabs")) {
-    				$("#item-editor").tabs("destroy");
+					$("#item-editor").tabs("destroy");
 				}
 				$("#item-editor").tabs({active: activeTab});
 				
 				// Add behaviour to submit core item updates 
 				$("#core-button").click(function () {
 					$.ajax(_ctx + "/rest/item/" + nodeKey + "/update/core", {
-							type: "POST",
-		    			cache: false,
-		    			data: {
-		    				name: $("#core-tab input[name='name']").val(),
-		    				simplename: $("#core-tab input[name='simplename']").val(),
-		    				published: $("#core-tab input[name='published']").is(':checked')
-		    			}, 
-		    			dataType: "json",
-		    			success: function(json, status, z) {
-		    				window.alert("Success");
-		    			}
+						type: "POST",
+						cache: false,
+						data: {
+							name: $("#core-tab input[name='name']").val(),
+							simplename: $("#core-tab input[name='simplename']").val(),
+							published: $("#core-tab input[name='published']").is(':checked')
+						}, 
+						dataType: "json",
+						success: function(json, status, z) {
+							showDialog("#dialog-update-success");
+						},
+						error: function(json, status, z) {
+							showDialog("#dialog-update-error");
+						},
 					});
 				});
 				
 				// Add behaviour to submit item field updates 
 				$("#field-button").click(function () {
 					$.ajax(_ctx + "/rest/item/" + nodeKey + "/update/fields", {
-							type: "POST",
-		    			cache: false,
-		    			data: getFieldsFormInputData(), 
-		    			dataType: "json",
-		    			success: function(json, status, z) {
-		    				window.alert("Success");
-		    			}
+						type: "POST",
+						cache: false,
+						data: getFieldsFormInputData(), 
+						dataType: "json",
+						success: function(json, status, z) {
+							showDialog("#dialog-update-success");
+						},
+						error: function(json, status, z) {
+							showDialog("#dialog-update-error");
+						},
 					});
 				});
 				
 				// Add behaviour to add new item 
 				$("#add-button").click(function () {
 					$.ajax(_ctx + "/rest/item/" + nodeKey + "/add", {
-							type: "POST",
-		    			cache: false,
-		    			data: {
-		    					template: $("#add-tab select[name='template']").val(),
-		    					itemtype: $("#add-tab select[name='itemtype']").val(),
-			    				name: $("#add-tab input[name='name']").val(),
-			    				simplename: $("#add-tab input[name='simplename']").val()
-		    			}, 
-		    			dataType: "json",
-		    			success: function(json, status, z) {
-		    				window.alert("Success");
-		    				window.location = _ctx + "/page/editor/" + json;
-		    			}
+						type: "POST",
+						cache: false,
+						data: {
+							template: $("#add-tab select[name='template']").val(),
+							itemtype: $("#add-tab select[name='itemtype']").val(),
+							name: $("#add-tab input[name='name']").val(),
+							simplename: $("#add-tab input[name='simplename']").val()
+						}, 
+						dataType: "json",
+						success: function(json, status, z) {
+							showDialog("#dialog-add-success");
+							window.location = _ctx + "/page/editor/" + json;
+						},
+						error: function(json, status, z) {
+							showDialog("#dialog-add-error");
+							window.location = _ctx + "/page/editor/" + json;
+						},
 					});
 				});
 				
 				// Add behaviour to trash an item 
 				$("#trash-button").click(function () {
-					if (window.confirm("This will delete the current item PLUS ALL descendant items. Are you sure you want to do this?")) {
-						$.ajax(_ctx + "/rest/item/" + nodeKey + "/trash", {
-								type: "POST",
-			    			cache: false,
-								data: {key: nodeKey}, 
-			    			dataType: "json",
-			    			success: function(json, status, z) {
-			    				window.alert("Success");
-			    				window.location = _ctx + "/page/editor/" + json;
-			    			}
-						});
-					}
+					var theDialog = $("#dialog-trash-confirm");
+					theDialog.dialog({
+						resizable: false,
+						height:200,
+						modal: true,
+						buttons: {
+							"Delete all items": function() {
+								$.ajax(_ctx + "/rest/item/" + nodeKey + "/trash", {
+									type: "POST",
+									cache: false,
+									data: {key: nodeKey}, 
+									dataType: "json",
+									success: function(json, status, z) {
+										theDialog.dialog("close");
+										window.location = _ctx + "/page/editor/" + json;
+									},
+									error: function(json, status, z) {
+										theDialog.dialog("close");
+										window.location = _ctx + "/page/editor/" + json;
+									}
+								});
+							},
+							Cancel: function() {
+								$(this).dialog("close");
+							}
+						}
+					});
 				});
 				
 				// Add behaviour to template & itemtype selectors 
@@ -105,51 +141,51 @@ $(function() {
 				});
 		  		
 				// Add a fancytree to identify child links 
-	 	    $("#linknav").fancytree({
-	 	    	source: {
-	 	    		url: _ctx + "/rest/leftnav/lazy/thread",
-	 	    		data: queryParams,
-	 	    		cache: false,
-	 	    		checkbox: true
-	 	    	},
-	 	    	lazyLoad: function(event, data) {
-	 	    		var node = data.node;
-	 	    		data.result = {
-	 	    			url: _ctx + "/rest/leftnav/lazy/one",
-	 	    			data: {key: node.key}
-	 	    		};
-	 	    	},
-	 	   		activate: function(event, data) {
-	 	   			var linkType = $("#addlinkdiv select[name='linktype']").val();
-	 	   			var linkName = $("#addlinkdiv input[name='linkname']").val();
-	 	   			var parentId = nodeKey;
-	 	   			var childId = data.node.key;
+				$("#linknav").fancytree({
+					source: {
+						url: _ctx + "/rest/leftnav/lazy/thread",
+						data: queryParams, /* TODO: Not sure this is right */
+						cache: false,
+						checkbox: true
+					},
+					lazyLoad: function(event, data) {
+						var node = data.node;
+						data.result = {
+							url: _ctx + "/rest/leftnav/lazy/one",
+							data: {key: node.key}
+						};
+					},
+					activate: function(event, data) {
+						var linkType = $("#addlinkdiv select[name='linktype']").val();
+						var linkName = $("#addlinkdiv input[name='linkname']").val();
+						var parentId = nodeKey;
+						var childId = data.node.key;
 	 	   			
-		 	 		  if (linkType != 'unknown') {
-		 	 	   	  	var selector = $("#sortable-links");
-		 	 	   	 		var copy = $("#link-template li").clone(true);
+						if (linkType != 'unknown') {
+							var selector = $("#sortable-links");
+							var copy = $("#link-template li").clone(true);
 		 	 	   	  	
-		 	 					$.ajax(_ctx + "/rest/item/" + childId + "/name", {
-		 	 						type: "POST",
-		 	 						cache: false,
-		 	 						dataType: "text",
-		 	 						success: function(itemName, status, z) {
-		 	 			   	  	copy.find("a").attr("href", _ctx + "/page/editor/" + childId).html(linkType + " (" + linkName + "): " + itemName);
-		 	 			   	  	copy.find("span.hide").html(parentId + "," + childId + "," + linkType + "," + linkName);
-		 	 			   	  	copy.appendTo(selector);
-		 	 						}
-		 	 					});
-		 	 			  }
-		 	 			  else {
-		 	 				  window.alert("Please choose a link type");
-		 	 			  }
-	 	   			}
-	 	    });
+							$.ajax(_ctx + "/rest/item/" + childId + "/name", {
+								type: "POST",
+								cache: false,
+								dataType: "text",
+								success: function(itemName, status, z) {
+									copy.find("a").attr("href", _ctx + "/page/editor/" + childId).html(linkType + " (" + linkName + "): " + itemName);
+									copy.find("span.hide").html(parentId + "," + childId + "," + linkType + "," + linkName);
+									copy.appendTo(selector);
+								}
+							});
+						}
+						else {
+							showDialog("dialog-choose-linktype");
+						}
+					}
+				});
 	 	    
 				// Show link addition form when 'Add link' button is clicked
-	 	    $("#addlink-button").click(function(e) {
-	 	    	$("#addlinkdiv").css("visibility", "visible");
-	 	    });
+				$("#addlink-button").click(function(e) {
+					$("#addlinkdiv").css("visibility", "visible");
+				});
 				
 				// Add behaviour to 'Save links' button 
 				$("#savelinks-button").click(function(e) {
@@ -159,17 +195,17 @@ $(function() {
 					$("#sortable-links li").each(function(index, li) {
 						parts = $(li).find("span.hide").html().split(",");
 						obj = {
-								parentId: parts[0],
-								childId: parts[1],
-								type: parts[2],
-								name: parts[3],
-								ordering: index
+							parentId: parts[0],
+							childId: parts[1],
+							type: parts[2],
+							name: parts[3],
+							ordering: index
 						};
 						links.push(obj);
 					});
 					
 					// Ajax call to save links to db 
-	  			$.ajax(_ctx + "/rest/links/" + nodeKey + "/save", {
+					$.ajax(_ctx + "/rest/links/" + nodeKey + "/save", {
 						type: "POST",
 						cache: false,
 						data: JSON.stringify(links), 
@@ -177,13 +213,11 @@ $(function() {
 						dataType: "text",
 						processData: false,
 						success: function(obj, status, z) {
-							// TODO: add dialog 
-							window.alert("Success");
+							showDialog("dialog-update-success");
 						},
 						error: function(obj, status, z) {
-							window.alert("Failure");
+							showDialog("dialog-update-error");
 						},
-
 					});
 				});
 				
@@ -192,12 +226,12 @@ $(function() {
 					$(this).parent().remove();
 				});
 				
-	 		  // Initialise sortable links 
-	 	    $( "#sortable-links" ).sortable();
-	 	    $( "#sortable-links" ).disableSelection();
+				// Initialise sortable links 
+				$( "#sortable-links" ).sortable();
+				$( "#sortable-links" ).disableSelection();
 			}
 		});
-  };
+	};
   
 	// Left navigation
 	var queryParams = {};
@@ -205,65 +239,76 @@ $(function() {
 		queryParams = {key: _editingItemId};
 	}
 		
-$("#leftnav").fancytree({
-	extensions: ["dnd"],
-	source: {
-		url: _ctx + "/rest/leftnav/lazy/thread",
-		data: queryParams,
-		cache: false,
-		checkbox: true
-	},
-	lazyLoad: function(event, data) {
-		var node = data.node;
-		data.result = {
-			url: _ctx + "/rest/leftnav/lazy/one",
-			data: {key: node.key}
-		};
-	},
-  dnd: {
-    autoExpandMS: 400,
-    focusOnClick: true,
-    preventVoidMoves: true, // Prevent dropping nodes 'before self', etc.
-    preventRecursiveMoves: true, // Prevent dropping nodes on own descendants
-    dragStart: function(node, data) {
-      return true;
-    },
-    dragEnter: function(node, data) {
-       return true;
-    },
-    dragDrop: function(node, data) {
-    	// hitMode can be one of: 'after', 'before', 'over' 
-//					TODO: WINDOW.CONFIRM - use a jquery dialog instead      	
-//         	if (window.confirm("Are you sure you want to move the selected item?")) {
-					$.ajax(_ctx + "/rest/item/" + data.otherNode.key + "/move", {
-						type: "POST",
-						cache: false,
-						data: {
-							targetId: node.key,
-							mode: data.hitMode
-						}, 
-						dataType: "json",
-						success: function(json, status, z) {
-							window.location = _ctx + "/page/editor/" + json;
+	$("#leftnav").fancytree({
+		extensions: ["dnd"],
+		source: {
+			url: _ctx + "/rest/leftnav/lazy/thread",
+			data: queryParams,
+			cache: false,
+			checkbox: true
+		},
+		lazyLoad: function(event, data) {
+			var node = data.node;
+			data.result = {
+				url: _ctx + "/rest/leftnav/lazy/one",
+				data: {key: node.key}
+			};
+		},
+		dnd: {
+			autoExpandMS: 400,
+			focusOnClick: true,
+			preventVoidMoves: true, // Prevent dropping nodes 'before self', etc.
+			preventRecursiveMoves: true, // Prevent dropping nodes on own descendants
+			dragStart: function(node, data) {
+				return true;
+			},
+			dragEnter: function(node, data) {
+				return true;
+			},
+			dragDrop: function(node, data) {
+				var theDialog = $("#dialog-move-confirm");
+				theDialog.dialog({
+					resizable: false,
+					height:200,
+					modal: true,
+					buttons: {
+						"Move item": function() {
+							$.ajax(_ctx + "/rest/item/" + data.otherNode.key + "/move", {
+								type: "POST",
+								cache: false,
+								data: {
+									targetId: node.key,
+									mode: data.hitMode
+								}, 
+								dataType: "json",
+								success: function(json, status, z) {
+									theDialog.dialog("close");
+									window.location = _ctx + "/page/editor/" + json;
+								},
+								error: function(json, status, z) {
+									theDialog.dialog("close");
+									window.location = _ctx + "/page/editor/" + json;
+								}
+							});
+						},
+						Cancel: function() {
+							$(this).dialog("close");
 						}
-					});
-			        
-				  // data.otherNode.moveTo(node, data.hitMode);
-// 	        }
-    }
-  },
-	activate: function(event, data) {
-		var tabName = $("li.ui-tabs-active").attr("aria-controls");
-		var tabNum = 0;
-		if (tabName == 'field-tab') {tabNum = 1;}
-		else if (tabName == 'links-tab') {tabNum = 2;}
-		else if (tabName == 'add-tab') {tabNum = 3;}
-		renderItemForms(data.node.key, tabNum);
-	}
-});		
+					}
+				});
+			}
+		},
+		activate: function(event, data) {
+			var tabName = $("li.ui-tabs-active").attr("aria-controls");
+			var tabNum = 0;
+			if (tabName == 'field-tab') {tabNum = 1;}
+			else if (tabName == 'links-tab') {tabNum = 2;}
+			else if (tabName == 'add-tab') {tabNum = 3;}
+			renderItemForms(data.node.key, tabNum);
+		}
+	});		
 
 	if (_editingItemId) {
 		renderItemForms(_editingItemId);
 	}
-	
 });
