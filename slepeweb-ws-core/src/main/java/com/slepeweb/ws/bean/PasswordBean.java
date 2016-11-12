a401f065c51ea4eaaa6787dddd93e8809b898526
gbutt <gbutt@NP900X4C>
2016-11-12 18:12:04 +0000

    enhanced password generator

--- slepeweb-ws-core/src/main/java/com/slepeweb/ws/bean/PasswordBean.java
+++ slepeweb-ws-core/src/main/java/com/slepeweb/ws/bean/PasswordBean.java
@@ -5,6 +5,8 @@ import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlRootElement;
 import javax.xml.bind.annotation.XmlType;
 
+import org.apache.commons.lang3.StringUtils;
+
 @XmlRootElement(name="password")
 @XmlAccessorType(XmlAccessType.FIELD)
 @XmlType(name = "password", namespace = "http://server.ws.slepeweb.com/", propOrder = {"seed", "password", "error"})
@@ -33,5 +35,32 @@ public class PasswordBean {
 
 	public void setError(String error) {
 		this.error = error;
+	}
+	
+	public String getChunked() {
+		int strlen = -1;
+		if (StringUtils.isNotBlank(this.password) && (strlen = this.password.length()) > 4) {
+			int a=0, b;
+			StringBuilder sb = new StringBuilder();
+			
+			do {
+				b = a + 4;
+				if (b > strlen) { 
+					b = strlen;
+				}
+				
+				sb.append(this.password.substring(a, b));
+				
+				if (b < strlen) {
+					sb.append("  ");
+				}
+				
+				a = b;
+			}
+			while (a < strlen);
+			
+			return sb.toString();
+		}
+		return getPassword();
 	}
 }
